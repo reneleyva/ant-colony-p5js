15a19384321f3ad648bbc0450bce2d539eac5d8b
Rene Guerra Leyva <extern.rene.guerra.leyva@vwfs.io>
2022-01-14 13:20:08 -0600

    Pheromones ready to test

--- AntColony.js
+++ AntColony.js
@@ -3,7 +3,6 @@ class Node {
         this.id = id;
         this.neighbours = {}
         this.position = createVector(0, 0);
-        this.visited = false; //TEST!
     }
 
     setPosition(x, y) {
@@ -38,8 +37,9 @@ class Graph {
     constructor(N) {
         this.nodes = [];
         for (let i = 0; i < N; i++) {
-            this.nodes.push(new Node(i))
+            this.nodes.push(new Node(i.toString()))
         }
+        this.pheromones = {};
     }
 
     /**
@@ -84,6 +84,52 @@ class Graph {
         return this.nodes[int(strId)];
     }
 
+    getEdgePheromone(i, j) {
+        // "i-j" es lo mismo que "j-i"
+        const key1 = i + "-" + j;
+        const key2 = j + "-" + i;
+        if (key1 in this.pheromones) return this.pheromones[key1];
+
+        return this.pheromones[key2];
+    }
+
+    addPheromoneToEdge(i, j, weight) {
+        const key1 = i + "-" + j;
+        const key2 = j + "-" + i;
+
+        if (key1 in this.pheromones) {
+            // Si key1 está key2 también
+            this.pheromones[key1].addValue(weight);
+            this.pheromones[key2].addValue(weight);
+
+        } else {
+            // Ninguna está
+            const pheromone = new Pheromone(); 
+            pheromone.addValue(weight);
+
+            this.pheromones[key1] = pheromone;
+            this.pheromones[key2] = pheromone;
+        }
+    }
+
+    //TESTING!
+    printPheromones() {
+        let res = {}
+        Object.keys(this.pheromones).forEach(key => {
+            const pher = this.pheromones[key];
+            res[key] = pher.getRealValue();
+        })
+
+        console.table(res);
+    } 
+
+    update() {
+        //Update Pheromones 
+        Object.keys(this.pheromones).forEach(key => {
+            this.pheromones[key].decay();
+        })
+    }
+
     display() {
         let pairsAlreadyDraw = {};
         this.nodes.forEach(node => {
@@ -93,7 +139,6 @@ class Graph {
             } else {
                 fill(0)
             }
-
             text(node.id, node.position.x, node.position.y)
 
             Object.keys(node.neighbours).forEach(neiId => {
@@ -109,78 +154,65 @@ class Graph {
     }
 }
 
-
-/**
- * Estructura de datos para buscar valor en tiempo O(1)
- * Agregar sin repeticiones in O(1)
- * Y hacer pop en O(1). 
- * Teniendo 2 copias de los mismos valores en un arreglo y un diccionario.
- */
-class SetStack {
+class Pheromone {
     constructor() {
-        this.array = [];
-        this.set = {};
+        this.decay_factor = 0.5;
+        this.real_value = 0;
+        this.costSum = 0; 
     }
 
-    size() {
-        return this.array.length
+    addValue(value) {
+        // Update value
+        const cost = 1/(value);
+        this.costSum += cost;
+        // console.log("cost", cost)
+
+        const currRealValue =  this.real_value;
+        this.real_value = ((1 - this.decay_factor) * currRealValue) + this.costSum;
     }
 
-    has(value) {
-        return value in this.set;
+    decay() {
+        //Decay Pheromone
+        const currRealValue = this.real_value;
+        this.real_value = ((1 - this.decay_factor) * currRealValue) + this.costSum;
     }
 
-    get(index) {
-        return this.array[index];
-    }
-
-    add(value) {
-        if (!(value in set)) {
-            this.array.push(value);
-            this.set[value] = true;
-        }
-    }
-
-    bulkAdd(array) {
-        array.forEach(value => this.add(value))
-    }
-
-    isEmpty() {
-        return this.array.length === 0;
-    }
-
-    pop() {
-        if (!this.isEmpty()) {
-            const value = this.array.pop();
-            delete this.set[value];
-            return value; 
-        }
+    getRealValue() {
+        return this.real_value;
     }
 }
 
-
 class Ant {
-    constructor(graph, initailNode) {
+    constructor(graph, initailNode, sourceOfFoodNode) {
         this.graph = graph;
+        this.homeNode = initailNode;
         this.startNode = initailNode;
+        this.position = this.startNode.position.copy();
+        this.speed = 8;
+
+        this.sourceOfFoodNode = sourceOfFoodNode;
+        this.arrivedAtSourceOfFood = false;
+
         //FOR TEST!
         this.startNode.visited = true;
 
-        this.position = this.startNode.position.copy();
-
-        this.setRandomNodeDestination();
-        this.nodesVisitedSetStack = new SetStack() // nodos que ha visitado
-        this.nodesVisitedSetStack.add(this.startNode.id); // Agregamos primero nodo como visitado
-
-        // Nodos a visitar
-        this.nodesToVisitSetStack = new SetStack();
-        this.nodesToVisitSetStack.bulkAdd(Object.keys(initailNode.neighbours))
+        this.nodesVisited = {} // nodos que ha visitado
+        this.nodesVisited[this.startNode.id] = true; // Agregamos primero nodo como visitado
 
         this.backTrackPath = [];
-        this.backTrackPath.push(this.startNode.id);
-        this.exploredAllNodes = false; // TEST! Exploró todos los nodos
+        //Decides a new destination from the start
+        const nextNode = this.chooseNextDestinationNode();
+        this.setDestinationByNode(nextNode);
     }
 
+    setDestinationByNode(node) {
+        this.destinationNode = node;
+        this.currDestinationVec = node.position.copy();
+    }
+
+    /**
+     * For testing
+     */
     setRandomNodeDestination() {
         const randomNeightId = random(Object.keys(this.startNode.neighbours)); 
         const randomNeigh = graph.getNodeById(randomNeightId);
@@ -190,15 +222,9 @@ class Ant {
     }
 
     display() {
-        if (this.exploredAllNodes) {
-            fill(0, 255, 0);
-        } else {
-            fill(255, 0, 0);
-        }
-
+        fill(255, 0, 0);
         stroke(0);
         ellipse(this.position.x, this.position.y, 5);
-
         // fill("green");
         // ellipse(this.currDestinationVec.x, this.currDestinationVec.y, 5);
     }
@@ -218,54 +244,122 @@ class Ant {
         return m
     }
 
-    setDestinationByNode(node) {
-        this.destinationNode = node;
-        this.currDestinationVec = node.position.copy();
+    
+
+    getPreviousNodeFromBackTrack() {
+        // Check if next is home and change variable
+
+        /*************
+         * Hacer prueba donde se checa si el proximo es vecino, sino lanzar error. 
+         */
+        const nextId = this.backTrackPath.pop();
+        if (nextId !== undefined) {
+            const nextNode = this.graph.getNodeById(nextId);
+            return nextNode;
+        }
     }
 
-    chooseNextDestinationNode() {        
+    getPreviousNodeToHome(neighboursIds) {
+        // Busca su casa
+        let nextNode;
+        if (neighboursIds.includes(this.homeNode.id)) {
+            nextNode = this.homeNode;
+        } else {
+            nextNode = this.getPreviousNodeFromBackTrack()
+        }
+        
+        const i = this.startNode.id;
+        const j = nextNode.id; 
+        const distance = this.startNode.position.dist(nextNode.position);
+        this.graph.addPheromoneToEdge(i, j, distance);
+
+        return nextNode;
+    }
+
+    isSomeNeighbourFood(neighboursIds) {
+        return neighboursIds.includes(this.sourceOfFoodNode.id)
+    }
+
+
+    chooseNextDestinationNode() {
         const neighboursIds = Object.keys(this.startNode.neighbours);
-        const notVisitedNeighbours = neighboursIds.filter(nId => !this.nodesVisitedSetStack.has(nId));
-        console.log('backtracking =>', this.backTrackPath)
+
+        if (this.arrivedAtSourceOfFood) {
+            return this.getPreviousNodeToHome(neighboursIds);
+        }
+
+        //IF Alguno de sus vecinos es comida? 
+            // Vamos a la comiuda
+            // Bandera de comida true
+        
+        //ELSE 
+            //EXPLORAR NORMAL, ver vecinos no visitados sino backtrack
+
+        if (this.isSomeNeighbourFood(neighboursIds)) {
+            this.arrivedAtSourceOfFood = true;
+            return this.sourceOfFoodNode;
+        }
+        
+        const notVisitedNeighbours = neighboursIds.filter(nId => !(nId in this.nodesVisited));
 
         if (notVisitedNeighbours.length > 0) {
             const nextId = random(notVisitedNeighbours);
             const newDestinationNode = this.graph.getNodeById(nextId);
-            this.setDestinationByNode(newDestinationNode);
-            this.backTrackPath.push(this.startNode.id);
-        } else {
-            // BackTrack hasta que encuentre vecinos sin explorar
-            const nextId = this.backTrackPath.pop();
-            if (nextId !== undefined) {
-                const nextNode = this.graph.getNodeById(nextId);
-                this.setDestinationByNode(nextNode)
-            } else {
-                console.log('ACABO!!!')
-            }
+            this.backTrackPath.push(newDestinationNode.id);
+            return newDestinationNode;
         }
 
+        return this.getPreviousNodeFromBackTrack();
     }
 
+    resetAnt() {
+        this.startNode = this.destinationNode;
+        this.arrivedAtSourceOfFood = false;
+        this.nodesVisited = {};
+        this.nodesVisited[this.startNode.id] = true;
+        this.backTrackPath = [];
+
+        const nextNode = this.chooseNextDestinationNode();
+        this.setDestinationByNode(nextNode);
+        this.graph.printPheromones();
+    }
+
+    printAnt() {
+        let res = {}; 
+        res["backtrack"] = this.backTrackPath.toString();
+        res["Visited"] = Object.keys(this.nodesVisited).toString();
+        res["CurrNode"] = this.startNode.id;
+        res["Destination"] = this.destinationNode.id;
+        res["FoundedFood?"] = this.arrivedAtSourceOfFood;
+        console.table(res);
+    }
+    
     update() {
         const distToDest = this.position.dist(this.currDestinationVec)
-        if(int(distToDest) >= 1) {
+        if(int(distToDest) >= this.speed) {
             const m = this.getDirectionVectorByDestination();
-            this.position.add(m.mult(1.2));
+            this.position.add(m.mult(this.speed));
         } else {
+            // this.printAnt();
+            if (this.destinationNode.id == this.homeNode.id) {
+                this.position = this.homeNode.position.copy();
+                this.resetAnt();
+                return;
+            };
+            
+            // this.graph.printPheromones(); // TO TEST! 
             // LLegó al nodo destino. Debemos elegir un próximo nodo            
             // Su nodo actual es el destino
             this.startNode = this.destinationNode;
+            this.nodesVisited[this.destinationNode.id] = true;
+
+            this.startNode.visited = true;
             this.position = this.currDestinationVec.copy();
-            
-            // FOR TEST!
-            this.destinationNode.visited = true;
 
-            //Agregamos el destino a los visitados
-            this.nodesVisitedSetStack.add(this.destinationNode.id);
 
-            // Elegimos un próximo nodo
-            this.chooseNextDestinationNode();
-            // this.setRandomNodeDestination();
+            //elegimos proximo nodo deacuerdo a su estado
+            const nextNode = this.chooseNextDestinationNode();
+            this.setDestinationByNode(nextNode);
         }
     }
 }
@@ -276,15 +370,19 @@ class AntColony {
         this.graph = graph;
         this.ants = [];
         // const initialNode = random(graph.nodes)
-        const initialNode = graph.nodes[3]
-        for (let i = 0; i < 1; i++) {
-            const ant = new Ant(graph, initialNode);
+        const initialNode = graph.nodes[4];
+        const sourceOfFoodNode = graph.nodes[5]; 
+        const NUMBER_OF_ANTS = 10; 
+
+        for (let i = 0; i < NUMBER_OF_ANTS; i++) {
+            const ant = new Ant(graph, initialNode, sourceOfFoodNode);
             this.ants.push(ant);
         }
     }
 
     display() {
         this.graph.display();
+        this.graph.update();
         this.ants.map(ant => ant.display());
     }
 
